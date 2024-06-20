556c3784f008ff0431a1c23d9063b22770c2f214
VadimLeonovv <leonovvadik0544@gmail.com>
2024-06-21 00:29:56 +0500

    Initial сommit

---
+++ laravel-7-shop-master/app/Http/Controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller {
+    public function __invoke(Request $request) {
+        $new = Product::whereNew(true)->latest()->limit(3)->get();
+        $hit = Product::whereHit(true)->latest()->limit(3)->get();
+        $sale = Product::whereSale(true)->latest()->limit(3)->get();
+//        dd($new->toArray());
+        return view('index', compact('new', 'hit', 'sale'));
+    }
+}
+
